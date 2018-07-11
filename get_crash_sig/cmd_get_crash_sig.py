a45e39f78ae7035e867a16c728a0d6d12fcf0130
Ben Wu <benjaminwu124@gmail.com>
2018-07-11 19:43:57 -0400

    setup.py and command line entrypoint

---
+++ get_crash_sig/cmd_get_crash_sig.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+import sys
+
+import ujson as json
+from siggen.generator import SignatureGenerator
+
+from get_crash_sig.symbolicate import symbolicate
+
+DESCRIPTION = """
+Takes raw crash trace and symbolicates it to return the crash signature
+"""
+
+
+def cmdline():
+    generator = SignatureGenerator()
+    crash_data = json.loads(sys.stdin.read())
+
+    symbolicated = symbolicate(crash_data)
+
+    signature = generator.generate(symbolicated)
+
+    print(signature)
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import codecs
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    'requests',
+    'siggen',
+    'ujson',
+]
+
+setup(
+    name='get-crash-sig',
+    version=0.1,
+    description=' Get crash signature from Firefox crash trace ',
+    long_description=codecs.open('README.md', encoding='utf-8').read(),
+    maintainer='Ben Wu',
+    maintainer_email='benjaminwu124@gmail.com',
+    url='https://github.com/Ben-Wu/get-crash-sig',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points="""
+        [console_scripts]
+        get-crash-sig=get_crash_sig.cmd_get_crash_sig:cmdline
+    """
+)
