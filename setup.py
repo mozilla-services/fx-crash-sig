60a61a2fdd6d0ed5cc1f8ad67d0eed7bce09d704
Ben Wu <benjaminwu124@gmail.com>
2018-07-11 22:13:00 -0400

    changes for pypi

--- setup.py
+++ setup.py
@@ -12,11 +12,12 @@ install_requires = [
 
 setup(
     name='fx-crash-sig',
-    version=0.1,
+    version='0.1.1',
     description=' Get crash signature from Firefox crash trace ',
     long_description=codecs.open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     maintainer='Ben Wu',
-    maintainer_email='benjaminwu124@gmail.com',
+    maintainer_email='bwub124@gmail.com',
     url='https://github.com/Ben-Wu/fx-crash-sig',
     packages=find_packages(),
     include_package_data=True,
@@ -24,5 +25,9 @@ setup(
     entry_points="""
         [console_scripts]
         fx-crash-sig=fx_crash_sig.cmd_get_crash_sig:cmdline
-    """
+    """,
+    classifiers=[
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 2 :: Only',
+    ]
 )
