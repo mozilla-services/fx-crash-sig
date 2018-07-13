019c01ea896d2ba4b58a06f58f61873c20ac27ff
Ben Wu <benjaminwu124@gmail.com>
2018-07-12 22:43:06 -0400

    License

--- fx_crash_sig/sample_traces.py
+++ fx_crash_sig/sample_traces.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 trace1 = {
     u'crash_info': {u'address': u'0x6b0d7be7',
                     u'crashing_thread': 0,
