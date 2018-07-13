9770cd7009e641844681823e2fff4bf84008f2e3
Ben Wu <benjaminwu124@gmail.com>
2018-07-13 14:16:23 -0400

    log to file

--- fx_crash_sig/utils.py
+++ fx_crash_sig/utils.py
@@ -1,7 +1,11 @@
 from __future__ import print_function
 
+import logging
 import sys
 
 
 def print_err(*args, **kwargs):
+    log_format = '%(asctime)-15s - %(message)s'
+    logging.basicConfig(filename='fx-crash-sig.log', format=log_format)
+    logging.error(args[0])
     print(*args, file=sys.stderr, **kwargs)
