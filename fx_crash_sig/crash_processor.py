c3e7dbc4faa1d64556285029b5799e969c584fcf
Will Kahn-Greene <willkg@mozilla.com>
2020-12-23 11:36:45 -0500

    Remove __future__ use and reorder some imports
    This removes __future__ use. We don't need that anymore since this is
    Python 3 only now.
    
    This reorders some imports so it's stdlib, then 3rd-party, then
    fx-crash-sig things.

--- fx_crash_sig/crash_processor.py
+++ fx_crash_sig/crash_processor.py
@@ -1,8 +1,10 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import json
 import sys
+
 from siggen.generator import SignatureGenerator
 
 from fx_crash_sig import SYMBOLS_API
--- fx_crash_sig/symbolicate.py
+++ fx_crash_sig/symbolicate.py
@@ -2,11 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from __future__ import print_function
+from itertools import islice
+import sys
 
 import requests
-import sys
-from itertools import islice
 
 from fx_crash_sig import SYMBOLS_API
 
