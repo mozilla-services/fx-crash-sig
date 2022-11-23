3f3b832b60befe422879fbe593d4aea63672dd50
Will Kahn-Greene <willkg@mozilla.com>
2022-11-23 12:40:13 -0500

    Add max version to request and remove ujson dependency (#58)

--- fx_crash_sig/example.py
+++ fx_crash_sig/example.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import ujson as json
+import json
 
 from fx_crash_sig import sample_traces
 from fx_crash_sig.crash_processor import CrashProcessor
