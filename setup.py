4a9af313494236f97f2e7e889b8ca847046240b5
Will Kahn-Greene <willkg@mozilla.com>
2022-11-22 21:04:21 -0500

    Add support for Python 3.10 and 3.11 (#41) (#42)
    This also updates the actions/setup-python and actions/checkout github
    action steps.

--- setup.py
+++ setup.py
@@ -40,6 +40,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Development Status :: 2 - Pre-Alpha",
     ]
 )
