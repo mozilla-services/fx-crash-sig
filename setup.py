7aa6b4eb8a27e424e3d1e62f2d88e7e0134d16e8
Will Kahn-Greene <willkg@mozilla.com>
2023-10-03 11:58:43 -0400

    Add support for Python 3.12 (#76)

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Development Status :: 2 - Pre-Alpha",
     ],
 )
